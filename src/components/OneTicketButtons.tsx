08273ada49ee9ea53fd75c3ccc63bff8d97e735d
Luis <simosa37@gmail.com>
2024-02-11 10:01:26 -0300

    fix oneTicketView prop error

--- src/components/OneTicketButtons.tsx
+++ src/components/OneTicketButtons.tsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-const OneTicketButtons = ({ ticketReset }: { ticketReset: () => void }) => {
+const OneTicketButtons = ({ ticketReset }: { ticketReset?: () => void }) => {
   const router = useRouter();
   return (
     <div className="mb-4 flex flex-col sm:flex-row gap-2 w-2/3 ">
       <button
         onClick={() => {
-          ticketReset();
+          if (ticketReset) {
+            ticketReset();
+          } else {
+            router.push("/scan");
+          }
           router.refresh();
         }}
         className="bg-white rounded-full px-6 py-4 w-full sm:max-w-xs text-red-500 text-center text-lg font-bold"
