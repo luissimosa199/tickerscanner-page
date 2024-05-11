bb4f65acda7e85606c09d8a16134870c5e81293d
Luis <simosa37@gmail.com>
2024-05-11 17:11:19 -0300

    Stats page fetch function imp.

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { cookies } from "next/headers";
 
-const StatsPage = () => {
+const TICKER_APP_URL = process.env.NEXT_PUBLIC_TICKER_APP_URL as string;
+
+const StatsPage = async () => {
+  const useCookies = cookies();
+  const token = useCookies.get("token");
+  const response = await fetch(`${TICKER_APP_URL}/stats/mainStats`, {
+    next: {
+      tags: ["stats"],
+    },
+    headers: {
+      Authorization: `Bearer ${token?.value}`,
+    },
+  });
+
+  const allStats = await response.json();
+
+  console.log(allStats);
+
   return <div>StatsPage</div>;
 };
 
